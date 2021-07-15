50e08a80f73b90517c5d0d62cffbb5a91f311b94
vprayag <84959964+vprayag@users.noreply.github.com>
2021-07-15 17:21:27 +0530

    Update AppOneApplication.java

--- src/main/java/com/skill/AppOneApplication.java
+++ src/main/java/com/skill/AppOneApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppOneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppOneApplication.class, args);
+		SpringApplication.run(AppOneApplication.class, args); 
 	}
 
 }
